46d0e14ff52d3d706b466570564537faad946f1d
Éder F. Zulian <zulian@eit.uni-kl.de>
2018-09-07 17:17:46 +0200

    SE mode 8 cores with different workloads

---
+++ arch/arm/arm_se_8_cores_8_workloads.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2018, University of Kaiserslautern
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+# TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER
+# OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# Author: Éder F. Zulian
+
+source ../../common/defaults.in
+source ../../common/util.in
+
+arch="ARM"
+mode="opt"
+gem5_elf="build/$arch/gem5.$mode"
+cd $ROOTDIR/gem5
+if [[ ! -e $gem5_elf ]]; then
+	build_gem5 $arch $mode
+fi
+
+benchmark_progs_path="$BENCHMARKSDIR/test-suite/SingleSource/Benchmarks/Stanford"
+benchmark_progs="
+Bubblesort
+FloatMM
+IntMM
+Oscar
+Perm
+Puzzle
+Queens
+Quicksort
+"
+config_script="configs/example/arm/starter_se.py"
+ncores="8"
+cpu_options="--cpu=hpi --num-cores=$ncores"
+currtime=$(date "+%Y.%m.%d-%H.%M.%S")
+output_rootdir="se_output_$currtime"
+mem_options="--mem-channels=1"
+#tlm_options="--tlm-memory"
+
+workload=""
+for b in $benchmark_progs; do
+	wl="$benchmark_progs_path/$b"
+	workload+="$wl "
+done
+
+output_dir="$output_rootdir/se_${ncores}_cores"
+$gem5_elf -d $output_dir $config_script $cpu_options $mem_options $tlm_options $workload &

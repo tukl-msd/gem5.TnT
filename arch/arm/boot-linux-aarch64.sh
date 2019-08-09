751ead2012c5cb4e4f17b3262450e4731062ce16
Éder F. Zulian <zulian@eit.uni-kl.de>
2019-08-09 08:39:49 +0200

    improvement

--- arch/arm/boot-linux-aarch64.sh
+++ arch/arm/boot-linux-aarch64.sh
@@ -46,22 +46,29 @@ sysver="20180409"
 syspath="$FSDIRARM/aarch-system-${sysver}"
 imgdir="${syspath}/disks"
 
-usage="Usage: $(basename "$0") {-h | DISK NUM_CPUS MEM_SIZE_GB}
-Boot Linux aarch64. Optionally, a DISK image can be specified.
+usage="Usage: $(basename "$0") {-h | [DISK NUM_CPUS MEM_SIZE_GB]}
+Boot Linux aarch64. Optionally the DISK image and the number of CPUs and the memory
+size in GiB can be specified.
 	-h    display this help and exit
 	DISK  raw disk image file (.img)
 	NUM_CPUS  number of CPUS
 	MEM_SIZE_GB memory size in GiB"
 
-if [ "$1" = "-h" ]; then
+if [ "$#" = "0" ]; then
+	img="$imgdir/linaro-minimal-aarch64.img"
+	ncpus="2"
+	mem_size="4GB"
+elif [ "$#" = "1" ] && [ "$1" = "-h" ]; then
 	echo "$usage"
 	exit 0
-elif [ "$#" != "3" ]; then
+elif [ "$#" = "3" ]; then
+	img="$1"
+	ncpus="$2"
+	mem_size="${3}GB"
+else
 	echo "$usage"
 	exit 1
 fi
-
-img="$1"
 
 if [ ! -e "$img" ]; then
 	printf "\n${Red}Error. File \"$img\" not found.${NC}\n\n"
@@ -73,13 +80,11 @@ disk_opts="--disk-image=$img"
 
 target="boot-linux-aarch64"
 config_script="configs/example/fs.py"
-ncpus="$2"
 cpu_clk="4GHz"
 machine_opts="--machine-type=VExpress_GEM5_V1"
 cpu_type="TimingSimpleCPU"
 #cpu_type="AtomicSimpleCPU"
 cpu_opts="--cpu-type=${cpu_type} --num-cpu=$ncpus --cpu-clock=${cpu_clk}"
-mem_size="${3}GB"
 mem_opts="--mem-size=${mem_size}"
 cache_opts="--caches --l2cache"
 #kernel="$ROOTDIR/gem5/vmlinux_aarch64"
